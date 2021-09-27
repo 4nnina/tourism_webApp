2d49ca0d1974c16173ea821ff7250accc61c0fce
4nnina <annadallave99@gmail.com>
2021-09-27 22:19:44 +0200

    login and logout

--- tourismApp/urls.py
+++ tourismApp/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('register', views.register, name="register"),
     path('logIn', views.logIn, name="logIn"),
+    path('logOut', views.logOut, name="logOut"),
     path('example/homepage', views.homepage, name="homepage"),
     path('example/', views.example, name='example'),
 ]
--- tourismApp/views.py
+++ tourismApp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from django.http import HttpResponse
 
 from .models import Crowding
@@ -13,8 +15,49 @@ def homepage(request):
     }
     return render(request, 'homepage.html', context)
 
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already used')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already used')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save();
+                return redirect('logIn')
+        else:
+            messages.info(request, 'Password not the same')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
+
 def logIn(request):
-    return render(request, 'logIn.html')
+    if request.method =='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credential invalid')
+            return redirect('logIn')
+    else:
+        return render(request, 'login_ex.html')
+
+def logOut(request):
+    auth.logout(request)
+    return redirect('/')
 
 def example(request):
     context = {

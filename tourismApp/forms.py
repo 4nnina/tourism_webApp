055fe892b64f20f04c9cc1653bca1496c6808df1
4nnina <annadallave99@gmail.com>
2021-11-10 17:49:14 +0100

    placeholder in input label

--- tourismApp/forms.py
+++ tourismApp/forms.py
@@ -15,6 +15,13 @@ class ArtForm(ModelForm):
 
         fields = ['name_it', 'descr_it', 'image_url', 'notes', 'open_time',
                   'tickets', 'vc', 'vc_id']
+
+        widgets = {
+            'name_it': forms.TextInput(attrs={'placeholder': 'Nome punto di interesse'}),
+            'image_url': forms.TextInput(attrs={'placeholder': "Url dell'immagine"}),
+            'vc_id': forms.TextInput(attrs={'placeholder': 'Identificativo per la verona card'}),
+        }
+
         labels = {
             'name_it': _('Name'),
             'descr_it': _('Description'),
@@ -37,6 +44,10 @@ class ArtForm_Trad(ModelForm):
             'descr_it': _('Description'),
         }
 
+        widgets = {
+            'name_it': forms.TextInput(attrs={'placeholder': 'Nome punto di interesse'}),
+        }
+
 class ArtForm_data(ModelForm):
     s_vc_perc = forms.FloatField( min_value=0, max_value=100)
 
@@ -49,13 +60,25 @@ class ArtForm_data(ModelForm):
             'vc': _('Verona Card')
         }
 
+        widgets = {
+            'image_url': forms.TextInput(attrs={'placeholder': "Url dell'immagine"}),
+            'vc_id': forms.TextInput(attrs={'placeholder': 'Identificativo per la verona card'}),
+        }
+
 class TourForm(ModelForm):
     class Meta:
         model = Tour
         #fields = ['name_it', 'type', 'descr_it', 'image_url', 'kml_path', 'duration', 'length']
         fields = ['name_it', 'type', 'descr_it', 'image_url', 'duration']
+
         labels = {
             'name_it': _('Name'),
             'descr_it': _('Description'),
             'image_url': _('Image Url'),
+        }
+
+        widgets = {
+            'name_it': forms.TextInput(attrs={'placeholder': 'Nome punto del tour'}),
+            'image_url': forms.TextInput(attrs={'placeholder': "Url dell'immagine"}),
+            'duration': forms.TextInput(attrs={'placeholder': 'Durata in ore'}),
         }
